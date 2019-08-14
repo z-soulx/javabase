b727b96bfd07d47edf0e6886943ac7911cd6cced
soulx <170918810@qq.com>
2019-08-14 22:08:16 +0800

    迭代器模式

--- src/main/java/com/study/soulx/utils/Test.java
+++ src/main/java/com/study/soulx/utils/Test.java
@@ -7,7 +7,17 @@ package com.study.soulx.utils;
  * @create: 2019-07-28 20:00
  **/
 public class Test {
+    private String s = "ss";
+    private  final String ss ;
+    Test(){
+        ss = "s";
+    }
     public static void main(String[] args) {
 
     }
+
+
+
+
+
 }
