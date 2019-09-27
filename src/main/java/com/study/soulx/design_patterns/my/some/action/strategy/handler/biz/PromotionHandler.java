a1df22bba821e46c643cd13664e923ff0ae00d51
soulx <zx170918810@163.com>
2019-09-27 14:05:10 +0800

    添加策略模式实战

---
+++ src/main/java/com/study/soulx/design_patterns/my/some/action/strategy/handler/biz/PromotionHandler.java
@@ -0,0 +1,23 @@
+package com.study.soulx.design_patterns.my.some.action.strategy.handler.biz;
+
+
+import com.study.soulx.design_patterns.my.some.action.strategy.handler.AbstractHandler;
+import com.study.soulx.design_patterns.my.some.action.strategy.handler.HandlerType;
+import com.study.soulx.design_patterns.my.some.action.strategy.model.OrderDTO;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author: soulx
+ * @Description: 促销订单处理器
+ * @Date: Created in 10:17 2019/2/2
+ */
+@Component
+@HandlerType("3")
+public class PromotionHandler extends AbstractHandler {
+
+    @Override
+    public String handle(OrderDTO dto) {
+        return "处理促销订单";
+    }
+
+}
--- src/main/java/com/study/soulx/test/T.java
+++ src/main/java/com/study/soulx/test/T.java
@@ -1,9 +1,14 @@
 package com.study.soulx.test;
 
 import com.study.soulx.design_patterns.my_code.chapter_6.NoCommand;
+import org.apache.commons.beanutils.BeanUtils;
+//import org.springframework.beans.BeanUtils;
+
 
 import java.io.FileNotFoundException;
-import java.util.Stack;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 
@@ -14,14 +19,51 @@ import java.util.stream.Stream;
  * @create: 2019-08-11 18:29
  **/
 public class T {
-    public static void main(String[] args) {
-    synchronized (T.class){
-        try {
-            T.class.wait();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+        T t = new T();
+        BitMap bitMap1 =  t.new BitMap(1);
+        bitMap1.setId(2);
+        BitMap bitMap2 =  t.new BitMap();
+        BeanUtils.copyProperties(bitMap2,bitMap1);
+        System.out.println(bitMap2);
     }
+    class BitMap {
+        Integer age;
+        Integer id;
+
+        public BitMap() {
+        }
+
+        public BitMap(Integer age) {
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            final StringBuilder sb = new StringBuilder("{");
+            sb.append("\"age\":")
+                    .append(age);
+            sb.append(",\"id\":")
+                    .append(id);
+            sb.append('}');
+            return sb.toString();
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
     }
 
 
