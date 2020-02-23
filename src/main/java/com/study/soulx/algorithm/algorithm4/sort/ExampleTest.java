219c9ba5d83c49d370b621a0e4e9b39228df9956
soulx <170918810@qq.com>
2020-02-23 20:43:23 +0800

    算法练习-排序

---
+++ src/main/java/com/study/soulx/algorithm/algorithm4/sort/ExampleTest.java
@@ -0,0 +1,27 @@
+package com.study.soulx.algorithm.algorithm4.sort;
+
+import edu.princeton.cs.algs4.*;
+
+/**
+ * @program: javabase
+ * @description: 排序模板
+ * @author: soulx
+ * @create: 2020-02-23 14:29
+ **/
+interface Sort{ //排序的基本模板
+    void sort(Comparable[] a);//排序
+    boolean less(Comparable v,Comparable w);//比较 v<w true
+    void exch(Comparable[] a,int i,int j);//交换
+    void show(Comparable a);//打印
+}
+/**
+* @Description: 官方排序 大致是性能递增 从shell开始就不是n^2级别了
+ * @see Selection,Insertion,Shell,Merge,Quick,Heap
+* @Author: soulx
+*/
+public class ExampleTest {
+    public static void main(String[] args) {
+
+
+    }
+}
